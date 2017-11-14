54071dcf46408e394d6486ab055e5d93c19ea8a6
Michelle Brien <michelle.brien12@gmail.com>
2017-11-14 13:47:01 -0200

    create new and update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,37 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   def destroy
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :done)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
-  get 'tasks/new'
-
-  get 'tasks/create'
-
-  get 'tasks/edit'
-
-  get 'tasks/update'
-
-  get 'tasks/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources:tasks
+
+  root to: 'tasks#index'
+
+
 end
